05a0c3d54d8b71181dbb7ca550379914d94392e3
Maat <maatthieu@gmail.com>
2020-05-23 16:54:18 +0200

    feat(client): add homepage

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import 'bootswatch/dist/slate/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/NavBar';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <main className="App">
       <Navbar/>
+      <div className="container pt-5">
+        <HomePage/>
+      </div>
     </main>
   );
 }
---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return ( 
+        <div className="jumbotron">
+            <h1 className="display-3">Bienvenue !</h1>
+            <p className="lead">Cliquez dans la barre de navigation pour commencer</p>
+        </div>
+     );
+}
+ 
+export default HomePage;
