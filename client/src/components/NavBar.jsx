6a64946c02fb186d5916a50d6d6257b721bddce6
Maat <maatthieu@gmail.com>
2020-05-23 16:49:46 +0200

    feat(client): cleaning and add Navigation bar

---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Navbar = () => {
+    return ( 
+
+        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+                <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+                <a className="nav-link" href="#">Liste de produits</a>
+            </li>
+            </ul>
+        </div>
+        </nav>
+
+     );
+}
+ 
+export default Navbar;
