cd644fcea2fb09597a9fa364362fa467a8b399df
Maat <maatthieu@gmail.com>
2020-05-23 18:39:26 +0200

    feat(product): handle product modif + errors

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -5,35 +5,67 @@ import ProductAPI from '../services/ProductAPI'
 
 const ProductPage = (props) => {
 
+    const {id = "new"} =  props.match.params //"new" if "id" is empty
+
+    const [isEditing, setIsEditing] = useState(false)
+
     const [product, setProduct] = useState({
         id: "",
         name: ""
     })
 
+    const [errors, setErrors] = useState({
+        id: "",
+        name: ""
+    })
+
+    const fetchProduct = async id => {
+        try {
+            const data = await ProductAPI
+                .getbyId(id)
+                .then(response => response.data)
+            setProduct(data)
+
+        } catch (error) {
+            console.log(error.response)
+        }
+    }
+
+    useEffect(() => {
+        if(id === "new") {
+            setIsEditing(false)
+        } else {
+            setIsEditing(true)
+            fetchProduct(id)
+        }
+    }, [id])
+
     const handleChange = ({currentTarget}) => {
         const { name, value } = currentTarget;
-        setProduct({ ...product, [name]: value });
+        setProduct({ ...product, [name]: value })
     };
 
     const handleSubmit = async event => {
         event.preventDefault()
         try {
-            await ProductAPI.add(product)
+            isEditing ? await ProductAPI.put(product) : await ProductAPI.add(product)
+            setErrors({})
         } catch (error) {
-            const message = error.response.data.errors.message;
-            console.log(message)
+            const message = error.response.data.errors.message
+            setErrors({...errors, name: message})
         }
+       
     }
 
     return (<>
-        <h1>Création d'un produit</h1> 
+         {isEditing ? <h1>Edition d'un produit</h1> : <h1>Création d'un produit</h1>}
         <form onSubmit={ handleSubmit }>
             <Field 
                 name= "name" 
                 label= "Name" 
                 value= {product.name} 
                 onChange={e => handleChange(e)}
-                error = ""
+                error = {errors.name} 
             />
             <div>
                 <button className="btn btn-primary" type="submit">Save</button>
--- client/src/services/ProductAPI.js
+++ client/src/services/ProductAPI.js
@@ -12,8 +12,18 @@ function add (product) {
     return axios.post("http://localhost:9000/api/v1/products/" + product.id, product);
 }
 
+function getbyId (id) {
+    return axios.get("http://localhost:9000/api/v1/products/" + id);
+}
+
+function put (product) {
+    return axios.put("http://localhost:9000/api/v1/products/" + product.id, product);
+}
+
 export default {
     findAll,
     delete: deleteProduct,
-    add
+    add,
+    getbyId,
+    put
 }
