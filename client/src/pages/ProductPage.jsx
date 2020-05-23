65a319eeee18ee3962d870617a0a742a7623c189
Maat <maatthieu@gmail.com>
2020-05-23 18:22:52 +0200

    feat(product): handle product creation

--- client/src/pages/ProductPage.jsx
+++ client/src/pages/ProductPage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {useState, useEffect} from "react"
 import Field from '../components/forms/Field'
+import ProductAPI from '../services/ProductAPI'
 
 const ProductPage = (props) => {
 
@@ -14,20 +15,30 @@ const ProductPage = (props) => {
         setProduct({ ...product, [name]: value });
     };
 
+    const handleSubmit = async event => {
+        event.preventDefault()
+        try {
+            await ProductAPI.add(product)
+        } catch (error) {
+            const message = error.response.data.errors.message;
+            console.log(message)
+        }
+    }
+
     return (<>
-    <h1>Création d'un produit</h1> 
-    <form>
-        <Field 
-            name= "name" 
-            label= "Name" 
-            value= {product.name} 
-            onChange={e => handleChange(e)}
-            error = ""
-        />
-        <div>
-            <button className="btn btn-primary" type="submit">Save</button>
-        </div>
-    </form> </>);
+        <h1>Création d'un produit</h1> 
+        <form onSubmit={ handleSubmit }>
+            <Field 
+                name= "name" 
+                label= "Name" 
+                value= {product.name} 
+                onChange={e => handleChange(e)}
+                error = ""
+            />
+            <div>
+                <button className="btn btn-primary" type="submit">Save</button>
+            </div>
+        </form> </>);
 }
  
 export default ProductPage;
--- client/src/services/ProductAPI.js
+++ client/src/services/ProductAPI.js
@@ -8,7 +8,12 @@ function deleteProduct (id) {
     return axios.delete("http://localhost:9000/api/v1/products/" + id)
 }
 
+function add (product) {
+    return axios.post("http://localhost:9000/api/v1/products/" + product.id, product);
+}
+
 export default {
     findAll,
-    delete: deleteProduct
+    delete: deleteProduct,
+    add
 }
