4e000fe64b057a6d44c426bc0b61c85fb6e0ddd9
Maat <maatthieu@gmail.com>
2020-05-23 16:34:39 +0200

    feat(server): install cors and change port

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 const router = require('./routes/createrRouter.js')();
 
 var isProduction = process.env.NODE_ENV === "production";
@@ -11,6 +12,7 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
