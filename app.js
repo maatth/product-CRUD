edc3b3c2dd568059dc68625704dafe341239f6c1
Maat <maatthieu@gmail.com>
2020-05-23 14:57:07 +0200

    feat(server): remove view

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -8,10 +7,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
